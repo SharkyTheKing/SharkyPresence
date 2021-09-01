15d1d0bb2180018bc15572045e599da2929105a6
Sharky <42697462+SharkyTheKing@users.noreply.github.com>
2021-08-31 19:23:10 -0600

    [Presence UI] Full UI view (#10)
    * Pre publish UI
    
    * Run formatter
    
    * Add in way to save client id
    
    * Create settings.pickle
    
    * Meh no big deal, client is public
    
    * Add in pathlib to correct path.
    
    * Update help and add in pathlib for icon
    
    * Remove comments
    
    * [Linter Setup] Add in lintering setups (#5)
    
    * Massive linter setups
    
    * Format
    
    * Add line at end of file
    
    * ....another end line...
    
    * Squashed commit of the following:
    
    commit d689e9fc4f23770bc2422dc46abe18db9538bb2b
    Merge: 1a75359 af4ab82
    Author: Sharky <42697462+SharkyTheKing@users.noreply.github.com>
    Date:   Mon Aug 30 20:06:28 2021 -0600
    
        Merge pull request #7 from SharkyTheKing/pr/update-helpmenu
    
        [HelpMenu] Update it slightly
    
    commit af4ab82f3d63af9b1741a3cf4c2d9dbcd7d56b06
    Author: Sharky <42697462+SharkyTheKing@users.noreply.github.com>
    Date:   Mon Aug 30 20:05:36 2021 -0600
    
        Remove comments
    
    commit 8a4f93931538541baab0362f406dcecd03811ba8
    Author: Sharky <42697462+SharkyTheKing@users.noreply.github.com>
    Date:   Mon Aug 30 20:04:12 2021 -0600
    
        Update help and add in pathlib for icon
    
    * If this somehow fixes it, ima be mad.
    
    * update git.ignore...
    
    * ...Wat?
    
    * [README] Information update (#6)
    
    * Update README.md
    
    Massive thanks to @fatflyingpigs
    
    * Whoopseh. Mah bad in copy-paste
    
    * Style fixes.
    
    I hope to god this is the only one it makes me do on this branch...
    
    * Format fix
    
    * [Remove comments] (#8)
    
    * test??
    
    * Remove comment
    
    * Fix setup
    
    * [UI Save] UI will now your entry. (#9)
    
    * UI save + Error Pop up
    
    * Add in helpmenu
    
    * Add in RPC variable, new button. remove quit button

---
+++ presence/presenceGUI.py
@@ -0,0 +1,373 @@
+import asyncio
+import pathlib
+import pickle
+import tkinter as tk
+from datetime import datetime, timedelta
+from tkinter import messagebox
+
+from pypresence import Presence
+
+NO_CLIENT_MESSAGE = {
+    "No Client ID": (
+        "No Client ID listed.\n"
+        "You MUST have a client_id from your application for this program to work.\n\n"
+        "Please read the README.md file to review how to get your ID."
+    )
+}
+
+# Look at https://www.geeksforgeeks.org/python-tkinter-validating-entry-widget/ for entry
+# https://stackoverflow.com/questions/4140437/interactively-validating-entry-widget-content-in-tkinter
+# menu https://www.geeksforgeeks.org/changing-the-colour-of-tkinter-menu-bar/
+# https://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html
+
+
+class PresenceGUI:
+    def __init__(self, master):
+        self.master = master
+        self.DICT_OF_FIELDS = {
+            "client_id": "Client ID",
+            "state": "State",
+            "details": "Details",
+            "large_image": "Large Image Name",
+            "large_text": "Large Image Text",
+            "small_image": "Small Image Name",
+            "small_text": "Small Image Text",
+            "buttons": "Buttons",
+        }
+        self.icon_path = pathlib.Path(__file__).parent / "data/myicon.ico"
+        self.master.title("Sharky PyPresence")
+        self.master.geometry("435x320")
+        self.master.configure(bg="gray")
+        self.master.iconbitmap(self.icon_path)
+
+        self.started_rpc = False
+        self.RPC = None
+        self.wait_until = None
+        self.restore_file = pathlib.Path(__file__).parent / "data/settings.pickle"
+        # https://stackoverflow.com/questions/33553200/save-and-load-gui-tkinter
+
+        self.start_building_widget()
+        self._restore_state()
+
+    def start_building_widget(self):
+        ents = self.presence_form(self.DICT_OF_FIELDS)
+
+        b1 = tk.Button(self.master, text="Process", command=(lambda e=ents: self.temp_rpc(e)))
+        b1.pack(side=tk.LEFT, padx=5, pady=5)
+
+        b2 = tk.Button(self.master, text="Stop", command=(lambda e=self.master: self._stop_rpc()))
+        b2.pack(side=tk.LEFT, padx=5, pady=5)
+
+        b3 = tk.Button(
+            self.master, text="Help", command=(lambda e=self.master: self.help_window())
+        )
+        b3.pack(side=tk.RIGHT, padx=5, pady=5)
+
+        # b4 = tk.Button(self.master, text="Quit", command=(lambda e=ents: self._save_state(e)))
+        # b4.pack(side=tk.LEFT, padx=5, pady=5)
+        self.master.wm_protocol("WM_DELETE_WINDOW", (lambda e=ents: self._save_state(e)))
+
+    def presence_form(self, fields):
+        entries = []
+        did_buttons = False
+        for key in fields.keys():
+            row = tk.Frame(self.master)
+            if key == "buttons":
+                if not did_buttons:
+                    # need to create 4 entries w/ different labels
+                    button_one_label = tk.Label(
+                        row, width=11, text="Button 1 Label", anchor="w", bg="gray"
+                    )
+                    button_one_link = tk.Label(
+                        row, width=11, text=" Button 1 Link", anchor="w", bg="gray"
+                    )
+                    b_One_label_entry = tk.Entry(row)
+                    b_One_link_entry = tk.Entry(row)
+
+                    button_two_label = tk.Label(
+                        row, width=11, text="Button 2 Label", anchor="w", bg="gray"
+                    )
+                    button_two_link = tk.Label(
+                        row, width=11, text=" Button 2 Link", anchor="w", bg="gray"
+                    )
+                    b_Two_label_entry = tk.Entry(row)
+                    b_Two_link_entry = tk.Entry(row)
+
+                    row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+
+                    button_one_label.grid(row=8, column=1, sticky=tk.W, rowspan=tk.YES)
+                    button_one_link.grid(row=8, column=3, sticky=tk.W, rowspan=tk.YES)
+                    b_One_label_entry.grid(row=8, column=2, padx=3, pady=1, rowspan=tk.YES)
+                    b_One_link_entry.grid(row=8, column=4, padx=3, pady=1, rowspan=tk.YES)
+
+                    button_two_label.grid(row=9, column=1, sticky=tk.W, rowspan=tk.YES)
+                    button_two_link.grid(row=9, column=3, sticky=tk.W, rowspan=tk.YES)
+                    b_Two_label_entry.grid(row=9, column=2, padx=3, pady=1, rowspan=tk.YES)
+                    b_Two_link_entry.grid(row=9, column=4, padx=3, pady=1, rowspan=tk.YES)
+
+                    entries.append(("Button 1 Label", b_One_label_entry))
+                    entries.append(("Button 1 Link", b_One_link_entry))
+                    entries.append(("Button 2 Label", b_Two_label_entry))
+                    entries.append(("Button 2 Link", b_Two_link_entry))
+
+                    did_buttons = True
+
+            elif key == "client_id":
+                reg = self.master.register(self._client_callback)
+                lab = tk.Label(row, width=15, text=fields[key], anchor="w", bg="gray")
+                ent = tk.Entry(row)
+                ent.config(validate="key", validatecommand=(reg, "%P"))
+                row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+                lab.pack(side=tk.LEFT)
+                ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+                entries.append((key, ent))
+
+            else:
+                lab = tk.Label(row, width=15, text=fields[key], anchor="w", bg="gray")
+                ent = tk.Entry(row)
+                row.pack(side=tk.TOP, fill=tk.X, padx=5, pady=5)
+                lab.pack(side=tk.LEFT)
+                ent.pack(side=tk.RIGHT, expand=tk.YES, fill=tk.X)
+                entries.append((key, ent))
+        return entries
+
+    def _client_callback(self, client_input):
+        """
+        Requires client_id to only be an int
+        """
+        if client_input.isdigit() or client_input == "":
+            return True
+        else:
+            return False
+
+    def help_window(self):
+        """
+        Displays help menu on start or when help is clicked.
+        """
+        helpwindow = tk.Toplevel(self.master)
+
+        helpwindow.title("Help Menu")
+        helpwindow.geometry("400x400")
+        helpwindow.config(bg="gray")
+        helpwindow.iconbitmap(self.icon_path)
+        helpwindow.resizable(width=False, height=False)
+
+        message = (
+            "Hello! If you are new to this program. "
+            "Please have a read at the README file on the repo. It"
+            " will help explain everything if it's not listed.\n\n"
+            "1. Head over to Discord's Developer Application Page.\n"
+            "Click `New Application`; give it a nice name. we're gonna use "
+            '"My Test Game" for ours. In discord\'s user pane, this will'
+            ' show as your status "Playing My Test Game"\n\n'
+            "2. Copy the `Application ID` from the website and paste "
+            "that value into the `Client ID` in our App.\n\n"
+            "3. In the website, on the left of the page, navigate to "
+            "`Rich Presence`. The `Cover Image` is not used and we can ignore it."
+            " Next, add a few images with the `Add Image(s)` button; rename the images "
+            "if needed, then click `Save Changes`. It may take several minutes for it to "
+            "properly save on discord's side.\n\n"
+            "4. In our App, the `Large Image Name` and `Small Image Name` can be set "
+            "to any image name that has been uploaded in the steps above.\n\n"
+            "5. The remaining fields in our app can be set to anything you desire."
+        )
+        label = tk.Label(helpwindow, text=message, bg="gray")
+        label.configure(wraplength=350, justify=tk.LEFT)
+        label.pack()
+        credit_label = tk.Label(helpwindow, text="Credits: SharkyTheKing", bg="gray")
+        credit_label.place(relx=0.0, rely=1.0, anchor="sw")
+        version_label = tk.Label(helpwindow, text="Version: 0.0.1", bg="gray")
+        version_label.place(relx=1.0, rely=1.0, anchor="se")
+
+    def _error_window(self, error: dict):
+        key_view = error.keys()
+        key_iterator = iter(key_view)
+        first_key = next(key_iterator)
+
+        # values_view = error.values()
+        # value_iterator = iter(values_view)
+        # first_value = next(value_iterator)
+        first_value = error[first_key]
+
+        messagebox.showerror(first_key, first_value)
+
+    def fetch(self, entries):
+        for entry in entries:
+            field = entry[0]
+            text = entry[1].get()
+            print('%s: "%s"' % (field, text))
+
+    def process_information(self, entries):
+        entry_dict = {
+            "client_id": False,
+            "state": False,
+            "details": False,
+            "large_image": False,
+            "large_text": False,
+            "small_image": False,
+            "small_text": False,
+            "buttons": [],
+        }
+        button_type = ["Button 1 Label", "Button 1 Link", "Button 2 Label", "Button 2 Link"]
+        button_dict = {"buttons": [{"label": "", "url": ""}, {"label": "", "url": ""}]}
+        for entry in entries:
+            field = entry[0]
+            text = entry[1].get()
+            if field in button_type:
+                if button_dict.get("buttons"):
+                    if field == "Button 1 Label":
+                        if not text:  # Since we require text on this...
+                            button_dict.pop("buttons")
+                            entry_dict.pop("buttons")
+                            continue
+                        else:
+                            button_dict["buttons"][0]["label"] = text
+                    elif field == "Button 1 Link":
+                        button_dict["buttons"][0]["url"] = text
+                    if button_dict.get("buttons"):
+                        if field == "Button 2 Label":
+                            if not text:
+                                button_dict["buttons"].pop(1)
+                            else:
+                                button_dict["buttons"][1]["label"] = text
+                        elif field == "Button 2 Link":
+                            if not text:
+                                try:
+                                    button_dict["buttons"].pop(1)
+                                except IndexError:
+                                    continue
+                            else:
+                                button_dict["buttons"][1]["url"] = text
+
+                    entry_dict["buttons"] = button_dict["buttons"]
+            else:
+                if text:
+                    entry_dict[field] = text
+                else:
+                    entry_dict.pop(field)
+        # print(entry_dict)
+        return entry_dict
+
+    def temp_rpc(self, entry_info):
+        if self.wait_until:
+            now = datetime.now()
+            if self.wait_until > now:
+                time_left = (self.wait_until - now).seconds
+                return self._error_window(
+                    {
+                        "Timeout Error": (
+                            "Please do not repeatedly push process.\n\n"
+                            "You must wait 15 seconds before you update your status.\n\n"
+                            "You have {seconds} seconds left to wait.".format(seconds=time_left)
+                        )
+                    }
+                )
+
+        if self.started_rpc is False:
+            entries = self.process_information(entry_info)
+            try:
+                self.RPC = Presence(entries["client_id"])
+                self.RPC.connect()
+            except KeyError:
+                return self._error_window(NO_CLIENT_MESSAGE)
+
+            entries.pop("client_id")
+
+        else:
+            entries = self.process_information(entry_info)
+            entries.pop("client_id")
+
+        now = datetime.now()
+        self.wait_until = now + timedelta(seconds=15)
+
+        self.RPC.update(**entries)
+        self.started_rpc = True
+
+    def _stop_rpc(self):
+        if not self.RPC:
+            return
+        self.RPC.close()
+        self.started_rpc = False
+
+    def _save_state(self, entry):
+        text = entry[0][1].get()  # client_id
+        # entry[0][1].delete(0, tk.END)
+        # entry[0][1].insert(0, "234235") # example.
+        data = {}  # "client_id": text}
+
+        for widget in entry:
+            label = widget[0]
+            text = widget[1].get()
+            data[label] = text
+
+        try:
+            with open(self.restore_file, "wb") as f:
+                pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            print("error saving state:", str(e))
+
+        self.master.quit()
+
+    def _restore_state(self):
+        """
+        Currently will only restore the state, everything else is manual
+        """
+        # https://stackoverflow.com/questions/6112482/how-to-get-the-tkinter-label-text
+        data = False
+        try:
+            with open(self.restore_file, "rb") as f:
+                data = pickle.load(f)
+        except FileNotFoundError:  # File will get created on save state anyways.
+            pass
+        except Exception as e:
+            print("error loading saved state:", str(e))
+
+        if not data:
+            return
+
+        self._restore_values(data)
+
+    def _restore_values(self, data):
+        """
+        Internal function to restore all entry state in UI
+        """
+        for child in self.master.winfo_children():
+            try:
+                label = child.winfo_children()[0]["text"]
+                entry = child.winfo_children()[1]
+                if label == "Client ID":
+                    entry.insert(0, data["client_id"])
+                elif label == "State":  # This works
+                    entry.insert(0, data["state"])
+                elif label == "Details":
+                    entry.insert(0, data["details"])
+                elif label == "Large Image Name":
+                    entry.insert(0, data["large_image"])
+                elif label == "Large Image Text":
+                    entry.insert(0, data["large_text"])
+                elif label == "Small Image Name":
+                    entry.insert(0, data["small_text"])
+                elif label == "Button 1 Label":
+                    entry.insert(0, data["Button 1 Label"])
+                elif label == "Button 1 Link":
+                    entry.insert(0, data["Button 1 Link"])
+                elif label == "Button 2 Label":
+                    entry.insert(0, data["Button 2 Label"])
+                elif label == "Button 2 Link":
+                    entry.insert(0, data["Button 2 Link"])
+            except (KeyError, IndexError, AttributeError):  # Since no point failing on these.
+                pass
+
+
+def main():
+    root = tk.Tk()
+    root.resizable(width=False, height=False)
+    Presence_Class = PresenceGUI(root)
+    try:
+        root.mainloop()
+    finally:
+        Presence_Class._stop_rpc()
+
+
+if __name__ == "__main__":
+    main()
